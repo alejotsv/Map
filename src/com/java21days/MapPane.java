e12b5a945a723bdbf2106105133d39484cb009b3
Alejandro Salas <asalas.wd@outlook.com>
2022-11-28 16:25:27 -0500

    Add Florida

--- src/com/java21days/MapPane.java
+++ src/com/java21days/MapPane.java
@@ -3,6 +3,7 @@ package com.java21days;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Arc2D;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Rectangle2D;
 
 public class MapPane extends JPanel {
@@ -25,5 +26,27 @@ public class MapPane extends JPanel {
                 comp2D.draw(wave);
             }
         }
+
+//        Draw Florida
+        GradientPaint gp = new GradientPaint(0F, 0F, Color.green, 350F, 350F, Color.orange, true);
+        comp2D.setPaint(gp);
+        GeneralPath fl = new GeneralPath();
+        fl.moveTo(10F, 12F);
+        fl.lineTo(234F, 15F);
+        fl.lineTo(253F, 25F);
+        fl.lineTo(261F, 71F);
+        fl.lineTo(344F, 209F);
+        fl.lineTo(336F, 278F);
+        fl.lineTo(295F, 274F);
+        fl.lineTo(259F, 188F);
+        fl.lineTo(211F, 171F);
+        fl.lineTo(195F, 174F);
+        fl.lineTo(191F, 118F);
+        fl.lineTo(120F, 56F);
+        fl.lineTo(94F, 68F);
+        fl.lineTo(81F, 49F);
+        fl.lineTo(12F, 37F);
+        fl.closePath();
+        comp2D.fill(fl);
     }
 }
