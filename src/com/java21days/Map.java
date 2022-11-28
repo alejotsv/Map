38e3c7d065aeb52a99d04d83692775d8f80be255
Alejandro Salas <asalas.wd@outlook.com>
2022-11-28 16:09:43 -0500

    Initial commit

---
+++ src/com/java21days/Map.java
@@ -0,0 +1,12 @@
+package com.java21days;
+
+import javax.swing.*;
+
+public class Map extends JFrame {
+    public Map(){
+        super("Map");
+        setSize(360, 350);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+}
---
+++ src/com/java21days/MapPane.java
@@ -0,0 +1,10 @@
+package com.java21days;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MapPane extends JPanel {
+    public void paintComponent(Graphics comp) {
+        Graphics2D comp2D = (Graphics2D) comp;
+    }
+}
